62fd6dbd8e88697a2646502c44886e5eec0949c6
Damian Radwański <damian.radwanski@famoc.com>
2025-09-02 10:18:02 +0200

    init

--- install.sh
+++ install.sh
@@ -9,6 +9,10 @@ install_dependencies()
     echo "starting..."
     dnf install -y httpd php php-fpm
     dnf -y install dnf-plugins-core
+    wget https://go.dev/dl/go1.25.0.linux-amd64.tar.gz
+    rm -rf /usr/local/go && tar -C /usr/local -xzf go1.25.0.linux-amd64.tar.gz
+    echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/profile
+    source /etc/profile
     if [[ $? -ne 0 ]]; then
         echo "install not ok"
         exit 0
